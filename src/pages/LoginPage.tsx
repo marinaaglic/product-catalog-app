51a57e699db2d367734b76953dcf53e6ed356308
marinaaglic <marinaaglic11@gmail.com>
2024-09-21 19:33:36 +0200

    styling added to login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
 import Input from "../components/reusable/Input";
+import "../styles/_loginPage.scss";
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className="form-wrapper">
       <form>
         <Input label="Username" id="username" className="login-input" />
         <Input label="Password" id="password" className="login-input" />
