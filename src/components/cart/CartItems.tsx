af6c8de0a1db809245455b2716d868d2186f1111
marinaaglic <marinaaglic11@gmail.com>
2024-09-22 01:19:53 +0200

    implemented adding to cart in context

--- src/components/cart/CartItems.tsx
+++ src/components/cart/CartItems.tsx
@@ -1,3 +1,16 @@
+import { useAuth } from "../../context/AuthContext";
+
 export default function CartItems() {
-  return <div>CartItems</div>;
+  const { removeFromCart, cartItems } = useAuth();
+
+  return (
+    <div>
+      {cartItems.map((item) => (
+        <ul key={item.id}>
+          <li>{item.title}</li>
+          <li>{item.price}</li>
+        </ul>
+      ))}
+    </div>
+  );
 }
