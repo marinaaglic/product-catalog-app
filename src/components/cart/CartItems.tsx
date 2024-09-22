9590384cdd579d9ecc321cb4123e80e241c3f586
marinaaglic <marinaaglic11@gmail.com>
2024-09-22 15:49:30 +0200

    styling changed for login page and cart items

--- src/components/cart/CartItems.tsx
+++ src/components/cart/CartItems.tsx
@@ -1,24 +1,25 @@
 import { useAuth } from "../../context/AuthContext";
 import { FaTrashAlt } from "react-icons/fa";
+import "../../styles/_cartItems.scss";
 
 export default function CartItems() {
   const { cartItems, removeFromCart } = useAuth();
 
   return (
-    <div>
+    <div className="cart-wrapper">
       {cartItems.length > 0 ? (
         cartItems.map((item) => (
-          <ul key={item.id}>
-            <li>
-              {item.title}
-              {item.quantity}
-              <FaTrashAlt
-                className="delete-icon"
-                onClick={() => removeFromCart(item.id)}
-              />
-            </li>
-            <li>{item.price}</li>
-          </ul>
+          <div key={item.id} className="items-div">
+            <p>{item.title} </p>
+            <p>
+              {item.price}${item.quantity}
+            </p>
+
+            <FaTrashAlt
+              className="delete-icon"
+              onClick={() => removeFromCart(item.id)}
+            />
+          </div>
         ))
       ) : (
         <p>Your cart is empty.</p>
