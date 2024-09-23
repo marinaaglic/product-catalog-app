79d90f2ab885b0bbced372aac3d1f81466ac7112
marinaaglic <marinaaglic11@gmail.com>
2024-09-23 03:06:29 +0200

    updated adding to cart logic

--- src/components/cart/CartItems.tsx
+++ src/components/cart/CartItems.tsx
@@ -3,27 +3,40 @@ import { FaTrashAlt } from "react-icons/fa";
 import "../../styles/_cartItems.scss";
 
 export default function CartItems() {
-  const { cartItems, removeFromCart } = useUserContext();
+  const { cartItems, removeFromCart, getTotalItems, getTotalAmount } =
+    useUserContext();
+
+  const totalItems = getTotalItems();
+  const totalAmount = getTotalAmount();
 
   return (
     <div className="cart-wrapper">
+      <div className="header">
+        <span>Product</span>
+        <span>Price</span>
+        <span>Quantity</span>
+      </div>
       {cartItems.length > 0 ? (
         cartItems.map((item) => (
           <div key={item.id} className="items-div">
-            <p>{item.title} </p>
-            <p>
-              {item.price}${item.quantity}
-            </p>
-
             <FaTrashAlt
               className="delete-icon"
               onClick={() => removeFromCart(item.id)}
             />
+            <p className="product-name">{item.title}</p>
+            <p className="product-price">${item.price.toFixed(2)}</p>
+            <p className="product-quantity">{item.quantity || 1}</p>
           </div>
         ))
       ) : (
         <p>Your cart is empty.</p>
       )}
+      <div className="total-products">
+        <span>Total products:</span>
+        <span>{totalItems}</span>
+        <span>Total amount:</span>
+        <span>${totalAmount.toFixed(2)}</span>
+      </div>
     </div>
   );
 }
